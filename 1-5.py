df970f4587d99cf7d23c91ce5845de8b9ffc7a1a
briankim05 <seojin.kim@cardigan.org>
2019-08-03 16:28:56 +0900

    This is my 1st upload

---
+++ 1-5.py
@@ -0,0 +1,15 @@
+def is_odd(a):
+    if a % 2 == 1:
+        print("odd")
+    else:
+        print("even")
+
+is_odd(3434559)
+
+def three(a):
+    if a % 3 == 0:
+        print("3의 배수")
+    else:
+        print("응 아니야")
+
+three(23233434454)
